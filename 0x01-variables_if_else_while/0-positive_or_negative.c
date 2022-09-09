c5ba4aa04f9beaf1a013232c2a85784a51997df2
Amadi <paulamadi39@gmail.com>
2022-09-09 04:59:47 -0700

    new correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
 		printf("%d is %s\n", n, "negative");
@@ -23,6 +23,6 @@ int main(void)
 	{
 		printf("%d is %s\n", n, "zero");
 	}
-	return (0)
+	return (0);
 }
 	
