18c0c1669edfdf0b1f3023cdd75773375735858b
Amadi <paulamadi39@gmail.com>
2022-09-13 07:47:05 -0700

    corrected times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,6 @@
  * Description - if n is less than  0 or greater thanb 15, print nothing
  * @n: int type number
  */
-
 void print_times_table(int n)
 {
 	int a = 0, b, c;
@@ -13,27 +12,37 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 	while (a <= n)
+	{
 		for (b = 0; b <= n; b++)
+		{
 			c = a * b;
 			if (c > 99)
+			{
 				_putchar(c / 100 + '0');
 				_putchar((c / 10 % 10) + '0');
 				_putchar(c % 10 + '0');
-			else if (c > 9)
+			}
+			else if	(c > 9)
+			{
 				_putchar(' ');
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
+			}
 			else if (b != 0)
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
+			}
 			else
 				_putchar(c + '0');
 			if (b != n)
+			{
 				_putchar(',');
 				_putchar(' ');
+			}
+		}
 		_putchar('\n');
 		a++;
+	}
 }
-
-
