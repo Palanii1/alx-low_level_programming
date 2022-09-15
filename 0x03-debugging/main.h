13b6a15b4d13e37e1a9d7cb6f445e1a1d0d6a2e3
Amadi <paulamadi39@gmail.com>
2022-09-15 09:45:57 -0700

    main.h updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int day, int month);
 int pritn_remaining_days(int day, int month, int year);
 
 #endif /*MAIN_H*/
