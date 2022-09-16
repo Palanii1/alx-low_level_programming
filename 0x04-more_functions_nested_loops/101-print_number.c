73451be22e29fc232473b10944bbdadd6267669c
Amadi <paulamadi39@gmail.com>
2022-09-16 02:35:26 -0700

    print an integer correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,55 +1,13 @@
 #include "main.h"
 
 /**
- * print_number - checks for a digit 0 thorugh 9
- * @n: n is variable
- * Return: Always 0;
+ * print_number - prints an integer
+ * @n: integer 
+ * Return: Always 0
  */
 
 void print_number(int n)
 {
-<<<<<<<<<<<<< HEAD
-	int i, d, f, p;
-	int j;
-	char num[1000];
-
-=============
-
-	unsigned int z;
-	int m, b;
->>>>>>>>>>>>> 02f4879befaee9c324521caa68f89e7cfec064bb
-
-	b = 10;
-	
-	if (n < 10 && n >= 0)
-	{
-		_putchar(n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar(n + '0');
-	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	_putchar(n + '0');
+	_putchar('\n');
 }
