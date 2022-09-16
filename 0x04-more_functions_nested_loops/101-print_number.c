51051b5e1c5761fe87fe62ed0e6119e83af55bfc
Amadi <paulamadi39@gmail.com>
2022-09-16 03:37:14 -0700

    corrected 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,12 +2,41 @@
 
 /**
  * print_number - prints an integer
- * @n: integer 
+ * @n: variable
  * Return: Always 0
  */
 
 void print_number(int n)
 {
-	_putchar(n + '0');
-	_putchar('\n');
+	int m, b;
+	unsigned int z;
+
+	b = 10;
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n - 2 * n;
+		_putchar('-');
+		_putchar(n + '0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar('-');
+		}
+		z = n;
+	while (z / b > 9)
+	{
+		m = z / b;
+		z = z % b;
+		_putchar(m + '0');
+		b = b / 10;
+	}
+	}
 }
