e149824b9a0ff4baee634999ea2ce444fc2f4722
Amadi <paulamadi39@gmail.com>
2022-10-03 13:11:21 -0700

    corrections to advanced ammloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,13 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0;
+	int i = 0;
 
-	while (s1[a] != '\0' && s2[a] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[a] != s2[a])
-			return (s1[a] - s2[a]);
-		a++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
@@ -54,7 +54,9 @@ int words_count(char *str)
 	while (*str)
 	{
 		if (*str != ' ')
+		{
 			flag = 1;
+		}
 		else if (flag == 1)
 		{
 			count++;
