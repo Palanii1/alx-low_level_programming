4ffbd7f8e179ab554e6bfe2b74fc04fd65247648
Amadi <paulamadi39@gmail.com>
2022-10-04 13:45:35 -0700

    new corrections to advanced tasks

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -57,20 +57,14 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
-
 				if (tmp == NULL)
 					return (NULL);
-
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-
 				matrix[k] = tmp - c;
-
 				k++;
-
 				c = 0;
 			}
 		}
