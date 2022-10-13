854cf68ccb1a3cfccb5565fa0b8082d734e9cc4e
Amadi <paulamadi39@gmail.com>
2022-10-13 06:48:25 -0700

    correction to first task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,9 +3,9 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - returns the sum of all its parameters
- * @n: unsigned int
- * Return: sum of argv
+ * sum_them_all - to return the sum of all its parameters
+ * @n: unsigned integer
+ * Return: 0 if n == 0
  */
 
 int sum_them_all(const unsigned int n, ...)
